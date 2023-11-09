294ad6500a3ecad048583329df3cf767845ce7e9
HanJingling <17860639535@163.com>
2023-11-09 16:00:25 +0800

    用户下单

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package com.sky.mapper;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.Orders;
+import com.sky.vo.OrderSubmitVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderMapper {
+
+    void insert(Orders orders);
+}
